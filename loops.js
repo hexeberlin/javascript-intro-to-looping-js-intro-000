d16722daa4469590d2085605e7afd5083a688ae2
Julia Miller <ihre.hexenhafte.hoheit@gmail.com>
2019-11-01 14:00:34 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,26 @@
+function forLoop(array){
+  for(let i=0; i<25; i++) {
+    if (i===1) array.push("I am 1 strange loop.")
+    else array.push("I am "+i+" strange loops.")
+  }
+  return array;
+}
+
+function whileLoop(number){
+  let countdown = number
+  while(countdown > 0) {
+    console.log (--countdown);
+  }
+  return "done"
+}
+
+function incrementVariable(i) {
+  i = i + 1;
+  return i;
+}
+
+function doWhileLoop(number){
+  do {
+    console.log("I run once regardless.")
+  } while (incrementVariable(0) < number)
+}
