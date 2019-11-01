003a79cd5b3e5a169f10c4281df1301ea701e84e
Julia Miller <ihre.hexenhafte.hoheit@gmail.com>
2019-11-01 14:07:50 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -14,13 +14,14 @@ function whileLoop(number){
   return "done"
 }
 
-function incrementVariable(i) {
-  i = i + 1;
-  return i;
-}
+
 
 function doWhileLoop(number){
   let i = 0;
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
   do {
     console.log("I run once regardless.")
   } while (incrementVariable(i) < number)
